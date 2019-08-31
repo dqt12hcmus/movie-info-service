f0fb54508d9c1090a5b23d1ccbe2128e46570450
thai <thai.do@codelink.io>
2019-08-31 14:55:07 +0700

    Add Movie controller, expose /movies/{movieId}

---
+++ src/main/java/com/thaido/movieinfoservice/controller/MovieController.java
@@ -0,0 +1,20 @@
+package com.thaido.movieinfoservice.controller;
+
+import com.thaido.movieinfoservice.model.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+  @RequestMapping("/{movieId}")
+  public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+
+    return new Movie(
+        "1",
+        "Transformer"
+    );
+  }
+}
